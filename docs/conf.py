cbc22035f7457ab46feb0f0c4743443decafb284
Andy Huynh <andyh1203@users.noreply.github.com>
2019-05-06 23:13:19 -0700

    Merge staging to master (#8)
    * Add black to CI and run on code base
    
    * Add black check to .travis.yml
    
    * Gather reqs from Pipfile / Pipfile.lock
    
    * Fix yml syntax error
    
    * Update setuptools from 41.0.0 to 41.0.1 (#4)
    
    * Update urllib3 from 1.24.3 to 1.25.2 (#6)
    
    * Update black to 19.3b0 (#5)
    
    * Update black from 18.9b0 to 19.3b0
    
    * Update black from 18.9b0 to 19.3b0
    
    * Bump version 0.1.2 -> 0.1.3

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2019, Andy Huynh"
 author = "Andy Huynh"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.2"
+release = "0.1.3"
 
 
 # -- General configuration ---------------------------------------------------
